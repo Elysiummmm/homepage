113df0e4edd000a8212eb2b11b0aefcfc03da5a1
Elysiummmm <57315331+Elysiummmm@users.noreply.github.com>
2025-09-04 15:59:46 +0200

    worked more on projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,17 +1,37 @@
 const projects = {
-    project1: { name: "project 1", content: "hi" },
-    project2: { name: "project 2", content: "hi" },
-    'wiss-casino': { name: "WISS Casino", content: "hi" },
+    'wiss-casino': {
+        name: "WISS Casino",
+        content: `
+        Casino Simulator created by me and a friend
+        (<a class="text-link" href="https://github.com/m4rv1n33" target="_blank"> <i class="bi bi-github"></i> @m4rv1n33 </a>)
+
+        `,
+        images: [ "https://media.discordapp.net/attachments/986005577316573286/1411778980956082206/image.png" ]
+    },
 };
 
 var selectorEntryCenters = {};
 
 const defaultProject = 'wiss-casino';
+const projectContents = document.getElementById('project-text');
+const projectImages = document.getElementById('project-images');
+const projectLinks = document.getElementById('project-links');
 const projectSelector = document.getElementById('project-selector-contents');
 const projectGap = parseInt(window.getComputedStyle(projectSelector).gap.slice(0, -2));
 
 function selectProject(projectID) {
+    let project = projects[projectID];
+
     projectSelector.style.translate = selectorEntryCenters[projectID] + "px";
+
+    projectContents.innerHTML = project.content;
+
+    project.images.forEach(image => {
+        let img = document.createElement('img');
+        img.setAttribute('source', image);
+
+        projectImages.appendChild(img);
+    });
 }
 
 function computeSelectorEntryCenters() {
